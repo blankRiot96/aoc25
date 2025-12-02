fb8f8c942ec1097f655f75c0ea679c5f0d44e81b
Axis <blankRiot96@gmail.com>
2025-12-03 03:01:54 +0530

    giga optimized part1

--- src/day02.rs
+++ src/day02.rs
@@ -12,52 +12,62 @@ fn get_ranges(input: &str) -> impl Iterator<Item = (u64, u64)> + '_ {
 }
 
 fn digits(num: u64) -> u64 {
-    if num == 0 {
-        return 1;
-    }
-
-    num.ilog10() as u64 + 1
+    (num.ilog10() + 1).into()
 }
+
+fn repeat(x: u64, n: u64, xlen: u64) -> u64 {
+    let mut res = 0;
+    let base = 10u64.pow(xlen as u32);
+    for i in 0..n {
+        let tenpow = base.pow(i as u32);
+        res += x * tenpow;
+    }
+    return res;
+}
+//
+// fn repeat(x: u64, n: u64, xlen: u64) -> u64 {
+//     let mut res = 0;
+//     let base = 10u64.pow(xlen as u32);
+//     for _ in 0..n {
+//         res = res * base + x;
+//     }
+//     res
+// }
 
 pub fn part_1() {
     let input = include_str!("inputs/day02.txt");
     let mut sum = 0u64;
     for (start, end) in get_ranges(input) {
-        for num in start..=end {
+        let mut num = start;
+        while num <= end {
             let n_digits = digits(num);
+            let half = n_digits / 2;
             if n_digits % 2 != 0 {
+                num = 10u64.pow(n_digits as u32) + 10u64.pow(half as u32);
                 continue;
             }
-
-            let half = n_digits / 2;
+            
             let p = 10u64.pow(half as u32);
             let right = num % p;
             let left = num / p;
             if left == right {
                 sum += num;
-            }
+            } 
+            let new = if right < left { left } else { left + 1 };
+            let d = digits(new);
+            num = repeat(new, 2, d);
+
+            
+            // println!("Next: {num}");
         }
     }
 
     println!("{sum}");
 }
 
-
-fn repeat(x: u64, n: u64, xlen: u64) -> u64 {
-    let mut res = 0;
-    let base = 10u64.pow(xlen as u32);
-    for i in 0..n {
-        let tenpow = base.pow(i as u32); 
-        res += x * tenpow;
-    }
-    return res;
-}
-
 fn first_n_digits(num: u64, num_len: u64, first_how_many: u64) -> u64 {
     num / 10u64.pow((num_len - first_how_many) as u32)
 }
-
-
 
 pub fn part_2() {
     let input = include_str!("inputs/day02.txt");
@@ -75,7 +85,9 @@ pub fn part_2() {
             }
 
             for n in 1..=(n_digits / 2) {
-                if repeat(first_n_digits(num, n_digits, n), n_digits / n, n) == num && !gathered.contains(&num) {
+                if repeat(first_n_digits(num, n_digits, n), n_digits / n, n) == num
+                    && !gathered.contains(&num)
+                {
                     sum += num;
                     gathered.insert(num);
                     continue 'numloop;
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod day01;
 mod day02;
 
 fn main() {
-    day02::part_1();
+    day02::part_2();
 }
