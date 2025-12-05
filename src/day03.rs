bd0562d342be71cd6e46c747f72c02d84dd8ddc0
Axis <blankRiot96@gmail.com>
2025-12-05 19:26:14 +0530

    d3p2 completed

--- src/day03.rs
+++ src/day03.rs
@@ -37,7 +37,7 @@ pub fn part_1(input: &str) -> u64 {
     sum
 }
 
-fn get_num(digits: Vec<&u8>) -> u128 {
+fn get_num(digits: Vec<u8>) -> u128 {
     digits.iter().rev().enumerate().fold(0, |sum, (i, &d)| {
         sum + ((d - b'0') as u128) * 10u128.pow(i as u32)
     })
@@ -46,46 +46,21 @@ fn get_num(digits: Vec<&u8>) -> u128 {
 pub fn part_2(input: &str) -> u128 {
     let mut total = 0u128;
 
-    let expected: [u128; 4] = [987654321111, 811111111119, 434234234278, 888911112111];
-    let mut gratzi = 0;
-    for bank in input.lines() {
-        println!("\n- - - -");
-        println!("Bank: {bank}");
-        let mut bank_digits = bank.as_bytes().iter();
-        let mut digits: Vec<u8> = Vec::with_capacity(12);
-        digits.push(*bank_digits.next().unwrap());
-
-        for (i, &d) in bank_digits.enumerate() {
-            if d > digits[0] {
-                if i == bank.len() - 2 && digits.len() == 11 {
-                    // println!("LOOK HERE: {}", d - b'0');
-                    digits.push(d);
-                } else {
-                    let placement = digits.len() - 1;
-                    println!("Digit: {}, Placement: {placement}", d - b'0');
-                    digits[placement] = d;
-                    for _ in 0..(digits.len() - placement - 1) {
-                        digits.pop();
-                    }
-                }
-            } else if digits.len() < 12 {
-                digits.push(d);
+    for bank in input.lines()  {
+        let k = 12;
+        let mut dro = bank.len() - k;
+        let mut digits: Vec<u8> = Vec::new();
+        for c in bank.as_bytes() {
+            while dro > 0 && digits.len() > 0 && digits.last().unwrap() < c {
+                digits.pop();
+                dro -= 1;
             }
+            digits.push(*c);
         }
-
-        let num: u128 = digits.iter().rev().enumerate().fold(0, |sum, (i, &d)| {
-            sum + ((d - b'0') as u128) * 10u128.pow(i as u32)
-        });
-        println!("Got:      {num}");
-        println!("Expected: {}", expected[gratzi]);
-
-        if num == expected[gratzi] {
-            println!("");
-        } else {
-            println!("");
-        }
+        
+        digits.truncate(k);
+        let num = get_num(digits);
         total += num;
-        gratzi += 1;
     }
 
     total
